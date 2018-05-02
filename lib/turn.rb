b7a8d5bdaebd226fa0aa2983fea1ce9a39c097b3
Taylor Williams <taylor@thewilliams.net>
2018-05-02 21:34:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def turn (board)
   puts "please enter 1-9:"
   index = gets.strip
-  if valid_move? (board, index)
+  if valid_move (board, index)
     move(board, index)
     display_board(board)
   else
